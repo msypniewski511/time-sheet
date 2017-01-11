262976b34f9f0760baf4a130e0eed22f59fe88ca
Maciej Sypniewski <msypniewski511@gmail.com>
2017-01-11 18:31:46 +0000

    fixtures

--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class EmployeesControllerTest < ActionController::TestCase
+  fixtures :departments, :grafiks, :shifts, :statuses
   setup do
     @employee = employees(:maciej)
+    @update = {
+      email: 'example2@gmail.com',
+      password: 'haslo',
+      shift_id: 2,
+      status_id: 2,
+      department_id: 2
+    }
   end
 
   test "should get index" do
@@ -17,11 +25,11 @@ class EmployeesControllerTest < ActionController::TestCase
   end
 
   test "should create employee" do
-    #assert_difference('Employee.count') do
-     # post :create, employee: { email: @employee.email, password: @employee.password, shift_id: @employee.shift_id, status_id: @employee.status_id, department_id: @employee.department_id }
-    #end
+    assert_difference('Employee.count') do
+      post :create, employee: @update
+    end
 
-    #assert_redirected_to employee_path(assigns(:employee))
+    assert_redirected_to employee_path(assigns(:employee))
   end
 
   test "should show employee" do
